8a47bbf5bddd26319a9c38d0b022786ffb63f4da
kevin <13855268721@163.com.com>
2024-04-14 09:04:05 +0800

    feat: 注释

--- packages/index.js
+++ packages/index.js
@@ -7,3 +7,13 @@ const install = function (Vue) {
 };
 
 export default {install}; // 这个方法以后再使用的时候可以被use调用
+
+/**
+ * 发包流程
+ * 1. 配置vite.config.js
+ * 2. 配置package.json
+ * 3. 打包 npm run build
+ * 4. npm login 浏览器打开提供地址进行登录
+ * 5. 如果失败：切回镜像 npm config set registry http://registry.npmjs.org/
+ * 6. npm publish
+ */
